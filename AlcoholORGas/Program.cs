6d882741b30f91d45e887ef3ed4a3ece3ffb7d2f
pedromedeiros21 <89794445+pedromedeiros21@users.noreply.github.com>
2024-02-09 15:17:36 -0300

    Add files via upload

---
+++ AlcoholORGas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AlcoholORGas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gasolina, alcool;
+            double divisao;
+            var reiniciar = true;
+
+            while (reiniciar)
+            {
+                Console.WriteLine("Digite o preço da gasolina");
+                gasolina = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite o preço do álcool");
+                alcool = Convert.ToDouble(Console.ReadLine());
+
+                divisao = alcool / gasolina;
+
+                if (divisao > 0.7)
+                {
+                    Console.WriteLine("Vale a pena abastecer com gasolina");
+                }
+                else if (divisao < 0.7)
+                {
+                    Console.WriteLine("Vale a pena abastecer com álcool");
+                }
+                else
+                {
+                    Console.WriteLine("Tanto faz com qual abastecer");
+                }
+
+                Console.Write("Deseja continuar? Sim/Não: ");
+                if (Console.ReadLine() == "Não")
+                {
+                    Console.WriteLine("Encerrando...");
+                    reiniciar = false;
+                }
+                else
+                {
+                    Console.WriteLine("Reiniciando...");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
